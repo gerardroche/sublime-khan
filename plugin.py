8a0f242fc758041690c5159aa0d4e2ed61ad72fe
Gerard Roche <gerardroche@users.noreply.github.com>
2023-08-08 13:33:07 +0100

    Fix clear when cursor at column 0

--- plugin.py
+++ plugin.py
@@ -5,7 +5,7 @@ class KhanCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, action):
         col = self.view.rowcol(self.view.sel()[0].begin())[1]
-        if col > 0:
+        if col >= 0:
             queue_save = False
             settings = self.view.settings()
             rulers = settings.get('rulers')
