757f1c05efc075b83fd61e7dfca2ae032e8db3a0
Gerard Roche <gerardroche@users.noreply.github.com>
2023-08-08 13:34:25 +0100

    refactor

--- plugin.py
+++ plugin.py
@@ -5,29 +5,31 @@ class KhanCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, action):
         col = self.view.rowcol(self.view.sel()[0].begin())[1]
-        if col >= 0:
-            queue_save = False
-            settings = self.view.settings()
-            rulers = settings.get('rulers')
+        if col < 0:
+            return
 
-            if action == 'add':
-                if col not in rulers:
-                    rulers.append(col)
-                    queue_save = True
-            elif action == 'remove':
-                if col in rulers:
-                    rulers.remove(col)
-                    queue_save = True
-            elif action == 'clear':
-                if rulers != []:
-                    rulers = []
-                    queue_save = True
+        queue_save = False
+        settings = self.view.settings()
+        rulers = settings.get('rulers')
+
+        if action == 'add':
+            if col not in rulers:
+                rulers.append(col)
+                queue_save = True
+        elif action == 'remove':
+            if col in rulers:
+                rulers.remove(col)
+                queue_save = True
+        elif action == 'clear':
+            if rulers != []:
+                rulers = []
+                queue_save = True
+        else:
+            raise Exception('unknown action')
+
+        if queue_save:
+            rulers.sort()
+            if rulers:
+                settings.set('rulers', rulers)
             else:
-                raise Exception('unknown action')
-
-            if queue_save:
-                rulers.sort()
-                if rulers:
-                    settings.set('rulers', rulers)
-                else:
-                    settings.erase('rulers')
+                settings.erase('rulers')
